b62a14eb3e0d4bd9b6cf3aa9863d289c610d37ea
Ghad <zimobie@gmail.com>
2020-04-23 16:47:28 +0100

    created mobile nav script file, adding script functions

---
+++ mobileNav.js
@@ -0,0 +1,27 @@
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('.close');
+
+const toggleMenu = () => {
+  const links = document.querySelector('.options');
+  console.log(links);
+  // if (links.style.display === 'block') {
+  //   links.style.display = 'none';
+  //   close.style.display = 'none';
+  // } else {
+  //   links.style.display = 'block';
+  //   close.style.display = 'block';
+  // }
+  if (links.classList.toggle('options-open')) {
+    links.classList.remove('options')
+  } else {
+    links.classList.add('options')
+  };
+};
+
+hamburger.addEventListener('click', () => {
+  toggleMenu();
+});
+
+close.addEventListener('click', () => {
+  !toggleMenu();
+});
